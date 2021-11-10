fdd31d1c9d968386cc7701cc9865891be7a0d199
Foodlike <dillibabu2015aj@gmail.com>
2021-11-10 20:46:49 +0530

    first

---
+++ src/org/forloop/basic.java
@@ -0,0 +1,18 @@
+package org.forloop;
+
+public class basic {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 100; i++) {
+			
+			System.out.println(i);
+			
+		}
+		
+			}
+	}
+		
+		
+		
+
